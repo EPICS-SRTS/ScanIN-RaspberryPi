386dbbaab546249f45b3e1d67cb9a205b25a29b6
Khalifa Alremeithi <kalremei@purdue.edu>
2019-03-06 18:25:12 +0000

    working falsesensor

--- FalseSensor.py
+++ FalseSensor.py
@@ -7,8 +7,8 @@ import binascii
 import time
 import datetime
 import sys
-import subprocess
-import thread
+import os
+import _thread
 from random import randint
 
 ### Helper functions
@@ -16,7 +16,7 @@ from random import randint
 # Execute the upload commands
 def pushPHP(data, time):
 	for id in data: # Recall data is an array/list of tag IDs
-		(output, err) = subprocess.Popen("php upload.php " + str(id), shell=False).communicate()
+		os.system("php /home/pi/ScanIN-RaspberryPi/upload.php " + str(id))
 	# Should be done!
 	print("Upload of " + str(len(data)) + " IDs complete.")
 		
@@ -24,19 +24,20 @@ def pushPHP(data, time):
 
 #  begin listening for incoming messages.
 
-while (true):
-	time.sleep(1000) # Every second, blast a number of tags
-	
+while (True):
+	time.sleep(5) # Every second, blast a number of tags
+	print("starting...")
 	consumedScans = []
 	processTime = time.localtime()
 	i = 0
 	t = randint(10,100)
-	while (i++ < t):
+	while i <= t:
 		tag = "thisIsNotRealData" + str(randint(0,9))+ str(randint(0,9))+ str(randint(0,9))+ str(randint(0,9))+ str(randint(0,9))+ str(randint(0,9))+ str(randint(0,9))
 		consumedScans.append(tag)
+		i += 1;
 		
 	# end while
 	# We now have all the data from the one waiting period.
 	# Execute the PHP shell command to process them.
 	# We'll do this on a new thread so that the scanner can continue working.
-	thread.start_new_thread(pushPHP, (consumedScans, processTime))
+	_thread.start_new_thread(pushPHP, (consumedScans, processTime))
