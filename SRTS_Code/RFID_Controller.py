e8a5ff5aad9a70b625ee2868d1f59e81baa98d21
Khalifa Alremeithi <kalremei@purdue.edu>
2019-04-04 19:11:49 -0400

    Updated

--- SRTS_Code/RFID_Controller.py
+++ SRTS_Code/RFID_Controller.py
@@ -14,87 +14,51 @@ from multiprocessing import Process, Queue
 # Open RFID Serial connection
 serialOpen = False
 while (not serialOpen):
-	try:
-		rfid_serial = serial.Serial(
-			port='/dev/ttyS0',
-			baudrate = 57600,
-			parity=serial.PARITY_NONE,
-			stopbits=serial.STOPBITS_ONE,
-			bytesize=serial.EIGHTBITS,
-			timeout=1
-		)
-		serialOpen = True
-	except:
-		with open("error.txt", "a") as errorFile:
-			e = sys.exc_info()[0]
-			errorFile.write("ERROR: %s\n" % e)
-
-#reset the database
-try:
-	reset_already()
-except:
-	with open("error.txt", "a") as errorFile:
-		e = sys.exc_info()[0]
-		errorFile.write("ERROR: %s\n" % e)
-		
-
-# initialize matrix
-if __name__ == '__main__':
-	q = Queue()
-	p = Process(target = launch_matrix, args=(q,))
-	p.start()
-
-time.sleep(2)
+    try:
+        rfid_serial = serial.Serial(
+            port='/dev/ttyS0',
+            baudrate=57600,
+            parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE,
+            bytesize=serial.EIGHTBITS,
+            timeout=1
+        )
+        serialOpen = True
+    except:
+        print("ERROR: %s\n" % e)
 
 # Check whether serial read RFID packet is valid
 def is_valid_RFID_Packet(RFID_Packet):
-	if RFID_Packet[0:6] == '1100ee':
-                print "valid"
-		return True
-	else:
-                print "bad"
-		return False
+    if RFID_Packet[0:6] == '1100ee':
+        print "valid"
+        return True
+    else:
+        print "bad"
+        return False
+
 
 def extract_tag_number(RFID_Packet):
-	return RFID_Packet[8:32]
-
-with open("out.txt", "a", 0) as outFile:
-	outFile.write("----------NEW INSTANCE----------------\n")
-
-prev_time = time.localtime()
-prev_day = prev_time.tm_yday
+    return RFID_Packet[8:32]
 
 while True:
-	try:
-		current_time = time.localtime()
-		current_day = current_time.tm_yday
+    try:
+        # Infinite loop to scan tags and run them against the DB
+        time.sleep(1)
+        bytesToRead = rfid_serial.inWaiting()
+        if (bytesToRead > 0):
+            # time.sleep(1)
+            RFID_Packet = rfid_serial.read(18)
+            RFID_Packet = binascii.hexlify(RFID_Packet)
+            print RFID_Packet
+            if is_valid_RFID_Packet(RFID_Packet):
+                tag_number = extract_tag_number(RFID_Packet)
+                print tag_number
+                process_scan(q, tag_number)
+                print(tag_number)
+                print("\n")
 
-		if (not prev_day == current_day):
-			reset_already()
-			prev_day = current_day
-			
-	
-		# Infinite loop to scan tags and run them against the DB
-		time.sleep(1)
-		bytesToRead = rfid_serial.inWaiting()
-		if (bytesToRead > 0):
-                        #time.sleep(1)
-			RFID_Packet = rfid_serial.read(18)
-			RFID_Packet = binascii.hexlify(RFID_Packet)
-			print RFID_Packet
-			if is_valid_RFID_Packet(RFID_Packet):
-				tag_number = extract_tag_number(RFID_Packet)
-				print tag_number
-				process_scan(q, tag_number)
-				with open("out.txt", "a") as outFile:
-					outFile.write(tag_number)
-					outFile.write("\n")
+            rfid_serial.flushInput()
 
-			rfid_serial.flushInput()
-
-	except:
-		with open("error.txt", "a") as errorFile:
-			e = sys.exc_info()[0]
-			errorFile.write("ERROR: %s\n" % e)
-
-
+    except:
+        e = sys.exc_info()[0]
+        print("ERROR: %s\n" % e)
