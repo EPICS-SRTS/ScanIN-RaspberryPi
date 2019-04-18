214515baf04701a4d1d8878349e9e8c2940d4e07
Eugene Lee <eugene.lee@live.com>
2019-04-18 19:32:17 -0400

    Update Sensor2PHP.py
    .

--- Sensor2PHP.py
+++ Sensor2PHP.py
@@ -87,7 +87,7 @@ while (serialOpen):
 		unconsumedBytes = unconsumedBytes - 1;
 		#try:
 		print("Looking for header: " + str(header));
-		if str(header) == "b'\xe2'":  # if this is a header byte...
+		if str(header) == "\xe2":  # if this is a header byte...
 			packet = rfid_serial.read(12)
 			packet = binascii.hexlify(packet)
 			print("Scanned: " + str(packet))
